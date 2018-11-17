d92e75ca7708226ba2a0b2a4563335bbd44ec310
Irindul <mathieu.regnard2@gmail.com>
2018-11-17 12:48:46 -0500

    Schema updated after migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_16_231656) do
+ActiveRecord::Schema.define(version: 2018_11_17_173204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 2018_11_16_231656) do
   create_table "users", force: :cascade do |t|
     t.string "pseudo"
     t.string "mail"
-    t.string "password"
+    t.string "password_digest"
     t.string "website"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
