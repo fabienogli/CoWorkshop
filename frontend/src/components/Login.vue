e720c4b8832ea3529c963c4d857801db504d2ae4
Irindul <mathieu.regnard2@gmail.com>
2018-11-20 11:42:59 -0500

    Base component for Login

---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="login">
+    <form @submit.prevent="login">
+      <input type="text"
+             placeholder="Email"
+             class="form-input email"
+             id="email"
+             v-model.trim="email">
+
+      <input type="password"
+             placeholder="Password"
+             class="form-input password"
+             id="password"
+             v-model.trim="password">
+      <button type="submit">Log in</button>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Login",
+    data() {
+      return {
+        email: "",
+        password: "",
+        errors: [],
+      }
+    },
+    methods: {
+      login() {
+        console.log('Logging in');
+        //Do something to log in
+      },
+    },
+  }
+</script>
+
+<style scoped>
+</style>
