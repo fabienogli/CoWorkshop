a98270b3c98bb4deb28f80e33ae5c27a4267cadf
Irindul <mathieu.regnard2@gmail.com>
2018-11-20 08:38:50 -0500

    New credentials youpi

--- server/config/initializers/knock.rb
+++ server/config/initializers/knock.rb
@@ -2,7 +2,8 @@ Knock.setup do |config|
 
   config.token_lifetime = 1.week
   config.token_signature_algorithm = 'HS256'
-  config.token_secret_signature_key = -> { Rails.application.secrets.secret_key_base }
+  p Rails.application.credentials.secret_key_base
+  config.token_secret_signature_key = -> { Rails.application.credentials.secret_key_base }
   config.not_found_exception_class_name = 'ActiveRecord::RecordNotFound'
   ## Expiration claim
   ## ----------------
