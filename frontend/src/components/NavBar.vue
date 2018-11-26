4b4e805aea4900670b7002e5330f49fe6d72a3aa
Irindul <mathieu.regnard2@gmail.com>
2018-11-26 15:17:33 -0500

    Added notifications

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -3,7 +3,13 @@
     <h1></h1>
     <ul>
       <li v-for="route in routes">
-        <router-link active-class="active" v-if="connected" class="custom" :to="{ name: route.name }">{{ route.text }}</router-link>
+        <router-link active-class="active" v-if="connected" class="custom"
+                     :to="{ name: route.name }">{{ route.text }}
+        </router-link>
+      </li>
+      <li class="notification">
+        <a class="notification-icon fa fa-bell" @click="openNotifications"></a>
+        <span class="badge" v-if="notifCount > 0">{{notifCount}}</span>
       </li>
       <li>
         <a @click="out" href="#">
@@ -34,10 +40,16 @@
       out() {
         logout();
       },
+      openNotifications() {
+
+      }
     },
     computed: {
       connected() {
-        return this.$store.getters['auth/token']  !== '';
+        return this.$store.getters['auth/token'] !== '';
+      },
+      notifCount() {
+        return 0;
       }
     },
     mounted() {
@@ -49,7 +61,7 @@
         if (route.meta.guest !== undefined) {
           return
         }
-        for (let i = 0; i < forbiddenArray.length; i ++) {
+        for (let i = 0; i < forbiddenArray.length; i++) {
           if (route.path === forbiddenArray[i]) {
             return
           }
@@ -60,7 +72,7 @@
   }
 </script>
 
-<style scoped>
+<style lang="scss">
 
   ul {
     list-style-type: none;
@@ -73,23 +85,38 @@
   li {
     float: left;
     display: inline;
+    a {
+      display: block;
+      color: #fff5fd;
+      text-align: center;
+      padding: 14px 16px;
+      text-decoration: none;
+      &:hover {
+        background-color: #111;
+      }
+    }
+    &:nth-last-child(-n+2) {
+      float: right;
+    }
   }
 
-  li a {
-    display: block;
-    color: #fff5fd;
-    text-align: center;
-    padding: 14px 16px;
-    text-decoration: none;
-  }
+  .notification {
+    position: relative;
 
-  li a:hover {
-    background-color: #111;
+    .badge {
+      position: absolute;
+      top: 1px;
+      right: 1px;
+      padding: 2px 5px;
+      border-radius: 50%;
+      background: red;
+      color: white;
+
+    }
   }
 
   .router-link-exact-active.active {
     background-color: black;
   }
-  li:last-child { float:right; }
 
 </style>
