009426f4e0dcc3c9186879edf4f907f91581bbc7
Irindul <mathieu.regnard2@gmail.com>
2018-11-28 08:23:14 -0500

    Style again, still ugly tho

--- frontend/src/components/NotificationDrawer.vue
+++ frontend/src/components/NotificationDrawer.vue
@@ -38,11 +38,39 @@
 <style lang="scss">
   @import "~@/styles/_variable";
   .notification-drawer {
+    display: flex;
+    height: 100%;
+    width: 60%;
+    margin: auto;
+    flex-direction: column;
+    padding-top: 30px;
     .notification-item {
+      padding-left: 10px;
+      padding-top: 10px;
+      border-bottom: 1px solid $lightGrey;
+      border-left: 1px solid $lightGrey;
+      border-right: 1px solid $lightGrey;
+      flex: 1;
+      max-height: 30px;
       cursor: pointer;
+
+      justify-content: center;
+      &:nth-child(2n) {
+        background-color: #ededed;
+      }
+      &:first-child {
+        border-top: 1px solid $lightGrey;
+        border-top-left-radius: 3px;
+        border-top-right-radius: 3px;
+      }
+      &:last-child {
+        border-bottom-left-radius: 3px;
+        border-bottom-right-radius: 3px;
+      }
+
     }
     .unread {
-      background: $lightGrey;
+      background: #787878;
     }
   }
 </style>
