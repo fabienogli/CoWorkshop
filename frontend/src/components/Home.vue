3ab38d18ad8095684d31ff732cb3e4b6e87c4e50
Fabien <fabien.ogli@etu.univ-lyon1.fr>
2018-11-28 15:56:40 -0500

    begin to do home

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <project-list :projects="projects"></project-list>
+    <!--<tag-list></tag-list>-->
+
   </div>
 </template>
 
 <script>
+
+  import ProjectList from '@/components/project/ProjectList'
+  // import TagList from "@/tag/TagList";
+
   export default {
     name: 'Home',
-    components: {},
+    components: {ProjectList},
     data() {
       return {
         msg: 'CoWorkshop'
       }
     },
+    computed: {
+      projects() {
+        return this.$store.getters['works/all'].filter(work => {
+          return work.user_id === this.user.id;
+        })
+      },
+      tags() {
+        return this.$store.getters['tags/all'].filter(tag => {
+          return tag.user_id === this.user.id;
+        })
+      },
+      user() {
+        return this.$store.getters['auth/user'];
+      }
+    }
   }
 </script>
 
